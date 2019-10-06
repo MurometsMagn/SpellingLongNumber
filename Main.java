4d236c44e9748970423c6ae020b092bb8878651e
MurometsMagn <45722996+MurometsMagn@users.noreply.github.com>
2019-10-06 19:13:12 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -41,17 +41,8 @@ public class Main {
         if (inputNumber == 0) {
             System.out.println("zero");
             System.exit(0);
-        } else if (inputNumber < 0) {
-            outputData += "minus ";
         }
         long correctableInputNumber = Math.abs(inputNumber); //в дальнейшем изменяем только это число
-
-
-        List<Integer> digits = new ArrayList<>();
-        while (correctableInputNumber != 0) {
-            digits.add((int) (correctableInputNumber % 10));
-            correctableInputNumber /= 10;
-        }
 
 
         //почему не получились константы?
@@ -60,48 +51,52 @@ public class Main {
                 "seventeen ", "eighteen ", "nineteen "};
         final String spellingTensList[] = new String[]{"", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ",
                 "seventy ", "eighty ", "ninety "};
-        final String spellingThousansList[] = new String[]{"", "", "thousand and ", "million and ", "billion and ",
-                "trillion and ", "quadrillion and "};
+        final String spellingThousansList[] = new String[]{"", "thousand ", "million ", "billion ",
+                "trillion ", "quadrillion "};
 
 
-        //создать переменную "количество троек, в т.ч. неполных", неполные наполнить нолями (максимум две)
-        //число разбиваем  на разряды по три цифры: единицы, десятки, сотни
-        int thousandsSeparators = digits.size() / 3;
-        if ((digits.size() % 3) > 0) {// одна или две значащих цифры в разряде
-            thousandsSeparators++;
-            digits.add(0);
-            digits.add(0);
-        }
+        //проход по числу тройками справа налево.
+        //как лучше оформить цикл? По итератору i или по correctableInputNumber?
+        boolean isAnd = false;
+        for (int i = 0; correctableInputNumber > 0; i++, correctableInputNumber /= 1000) {
+            int threeDigit;
+            String strThreeDigit = "";
 
-        for (int i = thousandsSeparators; i > 0; i--) {
-            int ones = digits.get(i * 3 - 3);
-            int tens = digits.get(i * 3 - 2);
-            int hundreds = digits.get(i * 3 - 1);
+            if (correctableInputNumber >= 1000) {
+                threeDigit = (int) correctableInputNumber % 1000;
+            } else {
+                threeDigit = (int) correctableInputNumber;
+            }
+
+            int hundreds = (threeDigit - (threeDigit % 100)) / 100;
+            int tens = (threeDigit - hundreds * 100 - (threeDigit % 10)) / 10;
+            int ones = threeDigit - hundreds * 100 - tens * 10;
 
             if (hundreds != 0) {
-                outputData = outputData.concat(spellingOnesList[hundreds] + "hundred ");
+                strThreeDigit = strThreeDigit.concat(spellingOnesList[hundreds] + "hundred ");
             }
+
             if ((tens == 0 && ones != 0) || tens == 1) {
-                outputData = outputData.concat(spellingOnesList[tens * 10 + ones]);
+                strThreeDigit = strThreeDigit.concat(spellingOnesList[tens * 10 + ones]);
             } else if (tens >= 2 && tens <= 9) {
-                outputData = outputData.concat(spellingTensList[tens] + spellingOnesList[ones]);
+                strThreeDigit = strThreeDigit.concat(spellingTensList[tens] + spellingOnesList[ones]);
             }
 
             if (ones != 0 || tens != 0 || hundreds != 0) {
-                outputData = outputData.concat(spellingThousansList[i]);
+                strThreeDigit = strThreeDigit.concat(spellingThousansList[i]);
             }
+
+            if (isAnd) {
+                strThreeDigit = strThreeDigit.concat("and ");
+            }
+            if (threeDigit != 0) {
+                isAnd = true;
+            } else isAnd = false;
+
+            outputData = strThreeDigit.concat(outputData);
         }
 
-        outputData = outputData.trim();
-        String[] words = outputData.split(" ");
-        String lastWord = words[words.length - 1];
-        if (lastWord.equals("and")) {
-            words[words.length - 1] = "";
-            outputData = "";
-            for (String word : words) {
-                outputData = outputData.concat(word + " ");
-            }
-        }
+        if (inputNumber < 0) outputData = "minus " + outputData; //конкатенацию?
 
         System.out.println(outputData);
     }
